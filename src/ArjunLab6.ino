0d2c7796d28e8948d6f48c18c90a457e014dd0a8
arjunkalbag <arjun.kalbag@writopialab.org>
2023-06-28 11:35:41 -0500

    Lab6FInal

--- src/ArjunLab6.ino
+++ src/ArjunLab6.ino
@@ -4,10 +4,6 @@ SYSTEM_THREAD(ENABLED);
 #include "oled-wing-adafruit.h"
 OledWingAdafruit display;
 
-uint64_t reading = analogRead(A4);
-double voltage = (reading * 3.3) / 4095.0;
-double temperature = (voltage - 0.5) * 100;
-double ftemp = (temperature * (9 / 5)) + 32;
 
 void setup()
 {
@@ -17,14 +13,18 @@ void setup()
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0, 0);
-  display.printf("%f C \n", temperature);
-  display.println(" ");
-  display.printf("%f F", ftemp);
-  display.display();
 }
 
 void loop()
 {
 
   uint64_t reading = analogRead(A4);
+  double voltage = (reading * 3.3) / 4095.0;
+  double temperature = (voltage - 0.5) * 100;
+  double ftemp = (temperature * 1.8) + 32;
+  display.printf("%f C \n", temperature);
+  display.println(" ");
+  display.printf("%f F \n", ftemp);
+  display.println(" ");
+  display.display();
 }
